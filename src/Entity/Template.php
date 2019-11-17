dfc72abf59acfc9ad51e0ce2fac8e6be57e8b8f2
Yacine Benfaid <yacine.benfaid@lydia-app.com>
2019-11-17 18:37:32 +0100

    refactor(TemplateManager): let the Template render itself
    Move all functions related to rendering to the Template object

--- src/Entity/Template.php
+++ src/Entity/Template.php
@@ -2,6 +2,7 @@
 
 class Template
 {
+    // todo make it private, but careful it's certainly used somewhere..
     public $id;
     public $subject;
     public $content;
@@ -11,5 +12,84 @@ class Template
         $this->id = $id;
         $this->subject = $subject;
         $this->content = $content;
+    }
+
+    public function renderFromExistingTemplate(Template $template, User $user, Quote $quote = null)
+    {
+        //todo still don't get it why we clone a template.
+        $template = clone($template);
+
+        $template->subject = $this->computeText($template->subject, $user, $quote);
+        $template->content = $this->computeText($template->content, $user, $quote);
+
+        return $template;
+    }
+
+    private function computeText($text, User $user, Quote $quote = null)
+    {
+
+        if ($quote) {
+            $text = $this->renderQuote($quote, $text);
+        }
+
+        if($this->hasPlaceHolder(PlaceHolders::USER_FIRST_NAME, $text)) {
+            $text = $this->replacePlaceHolder(
+                $text,
+                PlaceHolders::USER_FIRST_NAME,
+                $user->getFirstName()
+            );
+        }
+
+        return $text;
+    }
+
+    private function renderQuote(Quote $quote, string $text): string
+    {
+        $usefulObject = SiteRepository::getInstance()->getById($quote->siteId);
+        $destinationOfQuote = DestinationRepository::getInstance()->getById($quote->destinationId);
+
+        if ($this->hasPlaceHolder(PlaceHolders::QUOTE_SUMMARY_HTML, $text)) {
+            $text = $this->replacePlaceHolder(
+                $text,
+                PlaceHolders::QUOTE_SUMMARY_HTML,
+                Quote::renderHtml($quote)
+            );
+        }
+
+        if ($this->hasPlaceHolder(PlaceHolders::QUOTE_SUMMARY, $text)) {
+            $this->replacePlaceHolder(
+                $text,
+                PlaceHolders::QUOTE_SUMMARY,
+                Quote::renderText($quote));
+        }
+
+        if ($this->hasPlaceHolder(PlaceHolders::QUOTE_DESTINATION_NAME, $text)) {
+            $this->replacePlaceHolder($text, PlaceHolders::QUOTE_DESTINATION_NAME, $destinationOfQuote->countryName);
+        }
+
+        if ($this->hasPlaceHolder(PlaceHolders::QUOTE_DESTINATION_LINK, $text) ) {
+            $destination = DestinationRepository::getInstance()->getById($quote->destinationId);
+            if ($destination) {
+                $text = $this->replacePlaceHolder(
+                    $text,
+                    PlaceHolders::QUOTE_DESTINATION_LINK,
+                    $usefulObject->url . '/' . $destination->countryName . '/quote/' . $quote->id
+                );
+            } else {
+                $text = $this->replacePlaceHolder($text, PlaceHolders::QUOTE_DESTINATION_LINK, '');
+            }
+        }
+
+        return $text;
+    }
+
+    private function hasPlaceHolder(string $placeHolder, string $text)
+    {
+        return strpos($text, $placeHolder) !== false;
+    }
+
+    private function replacePlaceHolder(string $text, string $placeHolderToReplace, string $replacedBy)
+    {
+        return str_replace($placeHolderToReplace, $replacedBy, $text);
     }
 }
--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -5,90 +5,17 @@ class TemplateManager
     /** @var ApplicationContext */
     private $applicationContext;
 
-    public function getTemplateComputed(Template $tpl, array $data)
+    public function getTemplateComputed(Template $template, array $data)
     {
         $this->applicationContext = ApplicationContext::getInstance();
 
-        if (!$tpl) {
-            throw new \RuntimeException('no tpl given');
+        if (!$template) {
+            throw new \RuntimeException('no template given');
         }
-
-        $replaced = clone($tpl);
-        $replaced->subject = $this->computeText($replaced->subject, $data);
-        $replaced->content = $this->computeText($replaced->content, $data);
-
-        return $replaced;
-    }
-
-    private function computeText($text, array $data)
-    {
-        $quote = (isset($data['quote']) and $data['quote'] instanceof Quote) ? $data['quote'] : null;
-
-        if ($quote) {
-            $text = $this->renderQuote($quote, $text);
-        }
-
 
         $user  = (isset($data['user'])  and ($data['user']  instanceof User))  ? $data['user']  : $this->applicationContext->getCurrentUser();
+        $quote = (isset($data['quote']) and $data['quote'] instanceof Quote) ? $data['quote'] : null;
 
-        if($this->hasPlaceHolder(PlaceHolders::USER_FIRST_NAME, $text)) {
-            $text = $this->replacePlaceHolder(
-                $text,
-                PlaceHolders::USER_FIRST_NAME,
-                $user->getFirstName()
-            );
-        }
-
-        return $text;
-    }
-
-    private function renderQuote(Quote $quote, string $text): string
-    {
-        $usefulObject = SiteRepository::getInstance()->getById($quote->siteId);
-        $destinationOfQuote = DestinationRepository::getInstance()->getById($quote->destinationId);
-
-        if ($this->hasPlaceHolder(PlaceHolders::QUOTE_SUMMARY_HTML, $text)) {
-            $text = $this->replacePlaceHolder(
-                $text,
-                PlaceHolders::QUOTE_SUMMARY_HTML,
-                Quote::renderHtml($quote)
-            );
-        }
-
-        if ($this->hasPlaceHolder(PlaceHolders::QUOTE_SUMMARY, $text)) {
-            $this->replacePlaceHolder(
-                $text,
-                PlaceHolders::QUOTE_SUMMARY,
-                Quote::renderText($quote));
-        }
-
-        if ($this->hasPlaceHolder(PlaceHolders::QUOTE_DESTINATION_NAME, $text)) {
-            $this->replacePlaceHolder($text, PlaceHolders::QUOTE_DESTINATION_NAME, $destinationOfQuote->countryName);
-        }
-
-        if ($this->hasPlaceHolder(PlaceHolders::QUOTE_DESTINATION_LINK, $text) ) {
-            $destination = DestinationRepository::getInstance()->getById($quote->destinationId);
-            if ($destination) {
-                $text = $this->replacePlaceHolder(
-                    $text,
-                    PlaceHolders::QUOTE_DESTINATION_LINK,
-                    $usefulObject->url . '/' . $destination->countryName . '/quote/' . $quote->id
-                    );
-            } else {
-                $text = $this->replacePlaceHolder($text, PlaceHolders::QUOTE_DESTINATION_LINK, '');
-            }
-        }
-
-        return $text;
-    }
-
-    private function hasPlaceHolder(string $placeHolder, string $text)
-    {
-        return strpos($text, $placeHolder) !== false;
-    }
-
-    private function replacePlaceHolder(string $text, string $placeHolderToReplace, string $replacedBy)
-    {
-        return str_replace($placeHolderToReplace, $replacedBy, $text);
+        return $template->renderFromExistingTemplate($template, $user, $quote);
     }
 }
